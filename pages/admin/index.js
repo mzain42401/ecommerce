43daf97597002d9d518d16632a65b31494df70c3
m zain ali <mzain42401@gmail.com>
2023-10-21 15:50:46 +0500

    admin

---
+++ pages/admin/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <Link href='/admin/addProduct'>
+        add
+      </Link>
+    </div>
+  )
+}
+
+export default index
