bc998369e32af9ce11b18f5cf6e920b9b87a8192
m zain ali <mzain42401@gmail.com>
2023-10-26 18:02:02 +0500

    ui

--- pages/components/imageslider/ImageSlider.jsx
+++ pages/components/imageslider/ImageSlider.jsx
@@ -26,19 +26,10 @@ function ImageSlider() {
   
   const isFirstSlide = currentIndex === 0;
   const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-  const prevSlide = () => {
-    setCurrentIndex(newIndex);
-  };
+  
 
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
+ 
+  
   const sliderTomeout = setTimeout(sliderFunc, 4000);
   function sliderFunc() {
     setCurrentIndex(newIndex)
@@ -51,14 +42,14 @@ function ImageSlider() {
         className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
       ></div>
       {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      {/* <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div>
+      </div> */}
       {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      {/* <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
+      </div> */}
+      {/* <div className='flex top-4 justify-center py-2'>
         {slides.map((slide, slideIndex) => (
           <div
             key={slideIndex}
@@ -68,7 +59,7 @@ function ImageSlider() {
             <RxDotFilled />
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import ProductCard from '../components/productCard/ProductCard'
-
+import {BiSolidUpArrow,BiSolidDownArrow} from 'react-icons/bi'
 const index = () => {
+  const [drop,setDrop]=useState(false)
+
+  const dropCategory=()=>{
+    setDrop(!drop)
+  }
   return (
     <div>
       <Header/>
-      <div className='flex justify-center items-center  flex-wrap'>
+      <div onClick={dropCategory} className=' cursor-pointer absolute right-0 bg-[#013289] px-3 py-2 rounded-lg w-[200px] flex justify-between items-center  my-4 mr-4 hover:bg-[#0f4095]'>
+        <button className='  text-white  '>Search By Category </button>
+        <div className='text-white'>{
+          drop?<BiSolidUpArrow/>:<BiSolidDownArrow/>
+        } </div>
+        {
+          drop?<ul className='bg-white absolute left-0 top-10 mt-1 z-[99999] w-full rounded-lg py-2 px-4'>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+          <li className='py-1 m-1 hover:bg-gray-100'>Category</li>
+        </ul>:null
+        }
+        
+      </div>
+
+      <div className='flex justify-center items-center mt-16 flex-wrap'>
       <ProductCard/>
       <ProductCard/>
       <ProductCard/>
